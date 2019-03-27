2e058a501973d874c03ad42e7e5bb8e9eb6aaf11
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-27 11:08:25 +0100

    Freshness Caching Policy - Network First, Cache Last

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -1,9 +1,10 @@
 
 import {db} from "./database";
-
+import { HttpResponse } from "@angular/common/http";
+import { Response } from "express";
 
 export function readAllLessons(req, res) {
-
-    res.status(200).json({lessons:db.readAllLessons()});
-
+    (<Response>res).setTimeout(8000, () => {
+        (<Response>res).status(200).json({lessons:db.readAllLessons()});
+    });
 }
