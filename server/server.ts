f9fb07a0e6cfef7dc9436349e0cfc3482f33fa2a
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-28 12:40:34 +0100

    Update dependencies & server bug fixing

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,9 @@ import {Application} from "express";
 import {readAllLessons} from "./read-all-lessons.route";
 import {addPushSubscriber} from "./add-push-subscriber.route";
 import {sendNewsletter} from "./send-newsletter.route";
+import { AddressInfo } from 'net';
 const bodyParser = require('body-parser');
+import * as http from "http";
 
 const webpush = require('web-push');
 
@@ -46,8 +48,8 @@ app.route('/api/newsletter')
 
 
 // launch an HTTP Server
-const httpServer = app.listen(9000, () => {
-    console.log("HTTP Server running at http://localhost:" + httpServer.address().port);
+const httpServer: http.Server = app.listen(9000, () => {
+    console.log("HTTP Server running at http://localhost:" + (<AddressInfo>httpServer.address()).port);
 });
 
 
