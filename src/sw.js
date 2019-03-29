98571be0c8e8db7a1d704ead81ece6a759afd604
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-29 10:53:44 +0100

    Downloading The Offline Page - The Service Worker Installation Phase & View Service Workers HTTP Interception Features In Action & Service Workers Error Handling - Serving The Offline Page

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,41 @@
-const VERSION = "v5";
+const VERSION = "v15";
 
-function log(messages) {
-    console.log(VERSION, messages);
+function log(messages, ...data) {
+    console.log(VERSION, messages, data);
 }
 
-log("Installed Service Worker");
+async function installServiceWorker() {
+    log("Service worker installation started");
+    const request = new Request('/offline.html');
+    const response = await fetch(request);
+    log("response received after loading offline.html", response);
+    if (response.status !== 200) {
+        throw new Error("Could not load offline page");
+    }
+    const cache = await caches.open('app-cache');
+    cache.put(request, response);
+    log("Cached `offline.html`", cache);
+    log("Service worker installed");
+}
 
-self.addEventListener('install', () => {
-    log("Version is installed"); 
-});
+async function showOfflineError(event) {
+    let response;
+    try {
+        log("Calling network", event.request.url);
+        response = await fetch(event.request);
+    } catch(err) {
+        log("Network request failed. Serving offline page", err);
+        const cache = await caches.open('app-cache');
+        response = cache.match("/offline.html");
+    }
+    return response;
+}
+
+self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
 
 self.addEventListener('activate', () => {
-    log("Version is activated"); 
+    log("Service worker activation started");
+    log("Service worker activated"); 
 });
+
+self.addEventListener('fetch', event => event.respondWith(showOfflineError(event)));
